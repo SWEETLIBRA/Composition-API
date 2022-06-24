1c09583db8575cd7c2c2eba8b9e85b134fc58b38
Igor <igor,zhuiko@mail.ru>
2022-06-24 18:03:24 +0300

    loaded

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="container">
+    <form class="card" @submit.prevent="submit">
+      <h1>Auth</h1>
+
+      <h3 v-if="error">{{ error }}</h3>
+
+      <div class="form-control" 
+        :class="{invalid: !form.email.valid && form.email.touched}"
+      >
+        <label for="email">Email</label>
+        <input type="email" id="email" 
+          v-model="form.email.value" @blur="form.email.blur">
+        <small v-if="form.email.touched && form.email.errors.required"
+        >Ввведите Email</small>
+      </div>
+      
+      <div class="form-control" 
+        :class="{invalid: !form.password.valid && form.password.touched}"
+      >
+        <label for="password">Password</label>
+        <input type="password" id="password" 
+        v-model="form.password.value" @blur="form.password.blur">
+        <small v-if="form.password.touched && form.password.errors.required"
+        >Ввведите пароль</small>
+        <small v-else-if="form.password.touched && form.password.errors.minLength">
+          Пароль должен состоять не менее чем из 8 символов.Сейчас он состоит из
+          {{form.password.value.length}}</small>
+      </div>
+
+      <button class="btn primary" type="submit" :disabled="!form.valid">Submit</button>
+    </form>
+
+    <Suspense v-if="submitted">
+      <UserList />
+      <template #fallback>
+        <div class="loader"></div>
+      </template>
+    </Suspense>
+  </div>
+</template>
+
+<script>
+import {ref, onErrorCaptured} from 'vue'
+import UserList from './components/UserList'
+import {userForm} from './use/form'
+
+const required = val => !!val
+const minLength = num => val => val.length >= num
+
+export default {
+  setup() {
+    const submitted = ref(false)
+    const error = ref()
+    const form = userForm({
+      email: {
+        value: '',
+        validators: {required}
+      },
+      password: {
+        value: '',
+        validators: {required, minLength: minLength(8)}
+      }
+    })
+
+     function submit() {
+      submitted.value = true
+  }
+  onErrorCaptured(e => {
+    error.value = e.message
+  })
+    return {form, submit, submitted, error}
+  },
+  components: {UserList}
+}
+</script>
+
+<style>
+
+</style>
